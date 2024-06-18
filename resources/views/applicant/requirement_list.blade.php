4f9fe9b07fc79ed712cbec132561823840b7d8f1
ralph.sulit <ralph.sulit@iloverbs.com>
2024-06-18 10:18:29 +0800

    update

--- resources/views/applicant/requirement_list.blade.php
+++ resources/views/applicant/requirement_list.blade.php
@@ -1,20 +1,23 @@
 @extends('layout.applicant_layout')
 @section('content')
-    <div class="text-uppercase mt-4">List of Requirements</div>
+    <div class="container">
+        @include('applicant.forms.header')
 
-    <div class="border rounded border-dark m-0 p-0">
-        <ul class="mt-3">
-            <li class="p-2">
-                <div class="d-flex">
-                    <div class="col-7">Requirements Name</div>
-                    <div class="col-5">
-                        <input type="file" name="" id="" class="shadow form-controls" required>
+        <div class="text-uppercase mt-4">List of Requirements</div>
+        <div class="border rounded border-dark m-0 p-0">
+            <ul class="mt-3">
+                <li class="p-2">
+                    <div class="d-flex">
+                        <div class="col-7">Requirements Name</div>
+                        <div class="col-5">
+                            <input type="file" name="" id="" class="shadow form-controls" required>
+                        </div>
                     </div>
-                </div>
-            </li>
-        </ul>
-    </div>
-    <div class="my-3 gap-2 d-flex justify-content-end">
-        <div class="btn btn-warning" wire:click=''>Submit</div>
+                </li>
+            </ul>
+        </div>
+        <div class="my-3 gap-2 d-flex justify-content-end">
+            <div class="btn btn-warning" wire:click=''>Submit</div>
+        </div>
     </div>
 @endsection
