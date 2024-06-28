09cfd7ed9303344bd163a9db036ef6805ff17ea0
ralph.sulit <ralph.sulit@iloverbs.com>
2024-06-28 10:31:33 +0800

    update

--- resources/views/applicant/forms/result_exam.blade.php
+++ resources/views/applicant/forms/result_exam.blade.php
@@ -3,37 +3,27 @@
     <div class="container-fluid">
         <div class="container">
             @include('applicant.forms.header')
-
-<div class="my-5  rounded">
-    <div class="row">
-        <div class="col-md-6 d-flex flex-column justify-content-center pb-5 bg-secondary-subtle fs-3">
-            <div class="text-center">
-                <i class="fa-solid fa-box-open text-warning size "></i>
+            <div class="my-5  rounded">
+                <div class="row">
+                    <div class="col-md-6 d-flex flex-column justify-content-center pb-5 bg-secondary-subtle fs-3">
+                        <div class="text-center">
+                            <i class="fa-solid fa-box-open text-warning size "></i>
+                        </div>
+                        <div class="text-center mt-3">
+                            Your Exam result was
+                        </div>
+                        <div class="text-center text-semibold text-warning">
+                            10 <span class="text-dark"> out of</span> 100
+                        </div>
+                    </div>
+                    <div class="col-md-6 d-md-block d-none">
+                        <div class="d-flex justify-content-center">
+                            <img src="{{ asset('images/side2.avif') }}" alt="" class="img-fluid "
+                                style="max-width: 80%">
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div class="text-center mt-3">
-                Your Exam result was
-            </div>
-            <div class="text-center text-semibold text-warning">
-               10 <span class="text-dark"> out of</span> 100
-            </div>
-
         </div>
-        <div class="col-md-6 d-md-block d-none">
-            <div class="d-flex justify-content-center">
-                <img src="{{ asset('images/side2.avif') }}" alt="" class="img-fluid " style="max-width: 80%">
-            </div>
-        </div>
-
     </div>
-</div>
-
-
-
-
-<div class="my-3 gap-2 d-flex justify-content-end">
-    <div class="btn btn-secondary" wire:click=''><i class="fa-solid fa-angles-left me-2"></i>Back</div>
-    <div class="btn btn-warning" wire:click=''>Next<i class="fa-solid fa-angles-right ms-2"></i></div>
-</div>
-</div>
-</div>
 @endsection
